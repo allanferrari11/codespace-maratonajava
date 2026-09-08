2cee59a208f6b1492e11eb0560d286bd79f2ab24
allanferrari11 <allanferrari11@gmail.com>
2026-09-08 19:41:10 +0000

    Aprendendo a classe Date devido ao legado.

---
+++ maratonajava/rdates/test/DateTest01.java
@@ -0,0 +1,11 @@
+package maratonajava.rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date  = new Date(); //long de ms desde 01/01/1970
+        date.setTime(date.getTime() + 3_600_000);
+        System.out.println(date);
+    }
+}
